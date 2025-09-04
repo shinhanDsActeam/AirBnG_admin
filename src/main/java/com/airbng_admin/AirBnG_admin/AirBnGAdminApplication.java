79cff940e38a546209d996f502e75979b90b4966
standha <myyun02@naver.com>
2025-09-04 19:11:01 +0900

    admin commit

---
+++ src/main/java/com/airbng_admin/AirBnG_admin/AirBnGAdminApplication.java
@@ -0,0 +1,13 @@
+package com.airbng_admin.AirBnG_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnGAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnGAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airbng_admin/AirBnG_admin/AirBnGAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airbng_admin.AirBnG_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnGAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
